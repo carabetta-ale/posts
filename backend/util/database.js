629d91549bc19c68305a6d463aa6db577817e025
Alessandro Carabetta <alessandro.carabetta@crea-test.com>
2022-10-04 13:56:01 +0200

    added error handling for db connection

--- backend/util/database.js
+++ backend/util/database.js
@@ -9,5 +9,14 @@ const pool = mysql.createPool({
     password: config.password,
 });
 
+pool.getConnection(function(err, connection) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    connection.on('error', function(err) {
+      console.log(err);
+    });
+});
 
 module.exports = pool.promise();
